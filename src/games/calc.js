bacb8caef972348fed3bf3b37fa64181a740cc8e
Ekaterina Ivanova <ekaterinaivanova@Air-Ekaterina.Dlink>
2023-01-09 23:48:13 +0300

    add brain calc games

---
+++ src/games/calc.js
@@ -0,0 +1,34 @@
+import gameEngine from '../index.js';
+import getRandom from '../random.js';
+
+const descriptionGame = 'What is the result of the expression?';
+
+const calc = () => {
+  const operators = ['+', '-', '*'];
+  const number1 = getRandom(1, 25);
+  const number2 = getRandom(1, 25);
+  const randomOperators = operators[getRandom(0, operators.length - 1)];
+  const question = `${number1} ${randomOperators} ${number2}`;
+
+  let correctAnswer = 0;
+  switch (randomOperators) {
+    case '+':
+      correctAnswer = number1 + number2;
+      break;
+    case '-':
+      correctAnswer = number1 - number2;
+      break;
+    case '*':
+      correctAnswer = number1 * number2;
+      break;
+    default:
+      correctAnswer = null;
+  }
+
+  correctAnswer = correctAnswer.toString();
+  return [question, correctAnswer];
+};
+
+gameEngine(descriptionGame, calc);
+
+export default calc;
