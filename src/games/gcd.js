241fabae5c7cecf392cb768591f6c1491fb7cc39
Ekaterina Ivanova <ekaterinaivanova@Air-Ekaterina.Dlink>
2023-01-10 23:45:33 +0300

    add gcd game

--- src/games/gcd.js
+++ src/games/gcd.js
@@ -1,1 +1,25 @@
-import readlineSync from 'readline-sync';
+import gameEngine from '../index.js';
+import getRandom from '../random.js';
+
+const descriptionGame = 'Find the greatest common divisor of given numbers.';
+
+const gcd = () => {
+  const number1 = getRandom(1, 100);
+  const number2 = getRandom(1, 100);
+  const question = `${number1} ${number2}`;
+
+  const gcdNumber = (a, b) => {
+    if (a % b === 0) {
+      return b;
+    }
+    return gcdNumber(b, a % b);
+  };
+
+  const result = gcdNumber(number1, number2).toString();
+
+  return [question, result];
+};
+
+gameEngine(descriptionGame, gcd);
+
+export default gcd;
