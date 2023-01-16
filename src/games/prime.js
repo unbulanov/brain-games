3ca4ff22d1f1999d0848971249cb23d7a4bc8f16
Ekaterina Ivanova <ekaterinaivanova@Air-Ekaterina.Dlink>
2023-01-16 23:22:01 +0300

    add brain-prime

---
+++ src/games/prime.js
@@ -0,0 +1,27 @@
+import gameEngine from '../index.js';
+import getRandom from '../random.js';
+
+const descriptionGame = 'Answer "yes" if given number is prime. Otherwise answer "no".';
+
+const prime = () => {
+  const randomNumber = getRandom(1, 100);
+
+  const isPrime = (n) => {
+    if (n < 2) {
+      return false;
+    }
+
+    for (let i = 2, sqr = Math.sqrt(n); i <= sqr; i += 1) {
+      if (n % i === 0) {
+        return false;
+      }
+    }
+    return true;
+  };
+  const result = (isPrime(randomNumber) ? 'yes' : 'no');
+  return [randomNumber, result];
+};
+
+gameEngine(descriptionGame, prime);
+
+export default prime;
