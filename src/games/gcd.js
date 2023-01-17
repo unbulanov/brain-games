305d1c030454b40e0fb9c0498d8e4c2b1c0dee7c
Ekaterina Ivanova <ekaterinaivanova@Air-Ekaterina.Dlink>
2023-01-17 22:37:34 +0300

    add function findGcd

--- src/games/gcd.js
+++ src/games/gcd.js
@@ -3,23 +3,22 @@ import getRandom from '../random.js';
 
 const descriptionGame = 'Find the greatest common divisor of given numbers.';
 
-const gcd = () => {
+const findGcd = (a, b) => {
+  if (a % b === 0) {
+    return b;
+  }
+  return findGcd(b, a % b);
+};
+
+const getRound = () => {
   const number1 = getRandom(1, 100);
   const number2 = getRandom(1, 100);
   const question = `${number1} ${number2}`;
-
-  const gcdNumber = (a, b) => {
-    if (a % b === 0) {
-      return b;
-    }
-    return gcdNumber(b, a % b);
-  };
-
-  const result = gcdNumber(number1, number2).toString();
+  const result = findGcd(number1, number2).toString();
 
   return [question, result];
 };
 
-playGame(descriptionGame, gcd);
+const startGameGcd = () => playGame(descriptionGame, getRound);
 
-export default gcd;
+export default startGameGcd;
