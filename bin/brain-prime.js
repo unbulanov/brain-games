c29b3c36a53a759d3a07d08e77f3b48b2d120902
Ekaterina Ivanova <ekaterinaivanova@Air-Ekaterina.Dlink>
2023-01-17 22:46:09 +0300

    add function get round

--- bin/brain-prime.js
+++ bin/brain-prime.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
 
-import prime from '../src/games/prime.js';
+import startPrimeGame from '../src/games/prime.js';
 
-prime();
+startPrimeGame();
--- src/games/prime.js
+++ src/games/prime.js
@@ -3,25 +3,25 @@ import getRandom from '../random.js';
 
 const descriptionGame = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 
-const prime = () => {
-  const randomNumber = getRandom(1, 100);
+const isPrime = (n) => {
+  if (n < 2) {
+    return false;
+  }
 
-  const isPrime = (n) => {
-    if (n < 2) {
+  for (let i = 2, sqr = Math.sqrt(n); i <= sqr; i += 1) {
+    if (n % i === 0) {
       return false;
     }
+  }
+  return true;
+};
 
-    for (let i = 2, sqr = Math.sqrt(n); i <= sqr; i += 1) {
-      if (n % i === 0) {
-        return false;
-      }
-    }
-    return true;
-  };
+const getRound = () => {
+  const randomNumber = getRandom(1, 100);
   const result = (isPrime(randomNumber) ? 'yes' : 'no');
   return [randomNumber, result];
 };
 
-playGame(descriptionGame, prime);
+const startPrimeGame = () => playGame(descriptionGame, getRound);
 
-export default prime;
+export default startPrimeGame;
